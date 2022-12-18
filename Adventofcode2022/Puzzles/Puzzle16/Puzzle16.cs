44d71c7cf10b6eecc124bf26f938c5562799a335
SergeyRomanko <sergeyromanko@gmail.com>
2022-12-18 11:14:11 +0200

    wip

--- Adventofcode2022/Puzzles/Puzzle16/Puzzle16.cs
+++ Adventofcode2022/Puzzles/Puzzle16/Puzzle16.cs
@@ -55,9 +55,13 @@ namespace Adventofcode2022.Puzzles
         {
             var result = 0;
             var preasure = 0;
+            var open = new List<Node>();
 
-            for (var i = 0; i < 30; i++)
+            for (var i = 0; i < 2; i++)
             {
+                Console.WriteLine($"==== Minute {i} ====");
+                Console.WriteLine($"Open valves {string.Join(", ", open.Select(x => x.Name))} Preasure: {preasure}");
+                
                 result += preasure;
                 var nxt = GetNext(all, cur);
 
@@ -66,6 +70,13 @@ namespace Adventofcode2022.Puzzles
                 {
                     preasure += cur.Val;
                     cur.Val = 0;
+                    open.Add(cur);
+                    
+                    Console.WriteLine($"You open valve {cur.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"You move to valve  {nxt.Name}");
                 }
 
                 cur = nxt;
@@ -81,24 +92,65 @@ namespace Adventofcode2022.Puzzles
 
             if (cur.Val > 0)
             {
-                candidates.Add(cur);
+                candidates.Add(cur, EvaluateOpen(cur, all));
             }
-
+            
             return candidates
-                .OrderByDescending(x => EvaluateMove(x, all))
-                .First();
+                .OrderBy(x => x.Value)
+                .First()
+                .Key;
         }
 
         private int EvaluateMove(Node from, List<Node> all)
         {
-            return all
+            var tmp = all
                 .Where(x => x.Val > 0)
-                .Sum(x => (Distance(from, x, all) + 1) * x.Val);
+                .Select(x => (x.Name, Distance(from, x, all), 1, x.Val, 0))
+                .ToList();
+                 
+            for (var i = 0; i < tmp.Count; i++)
+            {
+                var value = tmp[i];
+                value.Item5 = (value.Item2 + value.Item3) * value.Item4;
+                tmp[i] = value;
+            }
+             
+            var result = tmp.Sum(x => (x.Item2 + x.Item3) * x.Item4);
+             
+            Console.WriteLine($" + EvaluateMove {from.Name} {result}");
+             
+            foreach (var v in tmp)
+            {
+                Console.WriteLine($"    - {v.Item1} {v.Item2, 2} {v.Item3, 2} {v.Item4, 2} {v.Item5, 3}");
+            }
+
+            return result;
         }
         
         private int EvaluateOpen(Node from, List<Node> all)
         {
+             var tmp = all
+                .Where(x => x.Val > 0)
+                .Select(x => (x.Name, Distance(from, x, all), (from == x ? 1 : 2), x.Val * (from == x ? -1 : +1), 0))
+                .ToList();
+                 
+             for (var i = 0; i < tmp.Count; i++)
+             {
+                 var value = tmp[i];
+                 value.Item5 = (value.Item2 + value.Item3) * value.Item4;
+                 tmp[i] = value;
+             }
+             
+             var result = tmp.Sum(x => (x.Item2 + x.Item3) * x.Item4);
+             
+             Console.WriteLine($" + EvaluateOpen {from.Name} {result}");
+             
+             foreach (var v in tmp)
+             {
+                 Console.WriteLine($"    - {v.Item1} {v.Item2, 2} {v.Item3, 2} {v.Item4, 2} {v.Item5, 3}");
+             }
 
+             return result;
         }
 
         private int Distance(Node from, Node to, List<Node> all)
@@ -119,13 +171,48 @@ namespace Adventofcode2022.Puzzles
 
                 foreach (var node in cur.Key.Tunnels)
                 {
-                    weights[node] = Math.Min(cur.Value + 1, weights[node]);
+                    if (weights.ContainsKey(node))
+                    {
+                        weights[node] = Math.Min(cur.Value + 1, weights[node]);
+                    }
                 }
 
                 weights.Remove(cur.Key);
             }
 
             return -1;
+        }
+        
+        public static IEnumerable<IEnumerable<T>> Permute<T>(this IEnumerable<T> sequence)
+        {
+            if (sequence == null)
+            {
+                yield break;
+            }
+        
+            var list = sequence.ToList();
+        
+            if (!list.Any())
+            {
+                yield return Enumerable.Empty<T>();
+            }
+            else
+            {
+                var startingElementIndex = 0;
+        
+                foreach (var startingElement in list)
+                {
+                    var index = startingElementIndex;
+                    var remainingItems = list.Where((e, i) => i != index);
+        
+                    foreach (var permutationOfRemainder in remainingItems.Permute())
+                    {
+                        yield return permutationOfRemainder.Prepend(startingElement);
+                    }
+        
+                    startingElementIndex++;
+                }
+            }
         }
     }
 }
