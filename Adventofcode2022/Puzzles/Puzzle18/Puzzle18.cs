8ed9455a6d4d296958ef39fa8f353afa50ef5923
SergeyRomanko <sergeyromanko@gmail.com>
2022-12-19 19:44:56 +0200

    Day 18.2

--- Adventofcode2022/Puzzles/Puzzle18/Puzzle18.cs
+++ Adventofcode2022/Puzzles/Puzzle18/Puzzle18.cs
@@ -36,14 +36,11 @@ namespace Adventofcode2022.Puzzles
                     .Select(x => x.Split(','))
                     .Select(x => new Vec3(int.Parse(x[0]), int.Parse(x[1]), int.Parse(x[2])))
                     .ToList();
-
-            var min = data
-                .Aggregate((a, b) => new Vec3(Math.Min(a.X, b.X), Math.Min(a.Y, b.Y), Math.Min(a.Z, b.Z)));
             
             var max = data
                 .Aggregate((a, b) => new Vec3(Math.Max(a.X, b.X), Math.Max(a.Y, b.Y), Math.Max(a.Z, b.Z)));
 
-            var model = new char[max.X+1, max.Y+1, max.Z+1];
+            var model = new char[max.X+3, max.Y+3, max.Z+3];
             
             for (var z = 0; z < model.GetLength(2); z++)
             {
@@ -58,27 +55,68 @@ namespace Adventofcode2022.Puzzles
             
             foreach (var vec3 in data)
             {
-                model[vec3.X, vec3.Y, vec3.Z] = '#';
+                model[vec3.X + 1, vec3.Y + 1, vec3.Z + 1] = '#';
             }
 
-            Print(model);
-            
             return new[]
             {
                 DoTask1(model).ToString(),
-                ""
+                DoTask2(model).ToString()
             };
+        }
+
+        private int DoTask2(char[,,] model)
+        {
+            var candidates = new Queue<Vec3>();
+            
+            candidates.Enqueue(new Vec3(0, 0, 0));
+
+            while (candidates.Count > 0)
+            {
+                var cur = candidates.Dequeue();
+
+                var neigh = Vec3.All
+                    .Select(x => new Vec3(cur.X + x.X, cur.Y + x.Y, cur.Z + x.Z))
+                    .Where(x => !IsOutside(model, x))
+                    .Where(x => model[x.X, x.Y, x.Z] == '.')
+                    .Where(x => !candidates.Contains(x));
+                
+                foreach (var pos in neigh)
+                {
+                    candidates.Enqueue(pos);
+                }
+                
+                model[cur.X, cur.Y, cur.Z] = '%';
+            }
+
+            for (var x = 0; x < model.GetLength(0); x++)
+            {
+                for (var y = 0; y < model.GetLength(1); y++)
+                {
+                    for (var z = 0; z < model.GetLength(2); z++)
+                    {
+                        if (model[x, y, z] == '.')
+                        {
+                            model[x, y, z] = '#';
+                        }
+                    }
+                }
+            }
+            
+            //Print(model);
+
+            return DoTask1(model);
         }
 
         private int DoTask1(char[,,] model)
         {
             var result = 0;
             
-            for (var z = 0; z < model.GetLength(2); z++)
+            for (var x = 0; x < model.GetLength(0); x++)
             {
                 for (var y = 0; y < model.GetLength(1); y++)
                 {
-                    for (var x = 0; x < model.GetLength(0); x++)
+                    for (var z = 0; z < model.GetLength(2); z++)
                     {
                         result += TestPosition(model, x, y, z);
                     }
@@ -95,36 +133,28 @@ namespace Adventofcode2022.Puzzles
                 return 0;
             }
 
+            return Vec3.All
+                .Select(dir => new Vec3(dir.X + x, dir.Y + y, dir.Z + z))
+                .Count(pos => IsOutside(model, pos) || model[pos.X, pos.Y, pos.Z] != '#');
+        }
+
+        private bool IsOutside(char[,,] model, Vec3 pos)
+        {
+            if (pos.X < 0 || pos.Y < 0 || pos.Z < 0)
+            {
+                return true;
+            }
+            
             var lengthX = model.GetLength(0);
             var lengthY = model.GetLength(1);
             var lengthZ = model.GetLength(2);
-
-            var result = 0;
-            
-            foreach (var dir in Vec3.All)
-            {
-                var pos = new Vec3(dir.X + x, dir.Y + y, dir.Z + z);
-
-                if (pos.X < 0 || pos.Y < 0 || pos.Z < 0)
-                {
-                    result++;
-                    continue;
-                }
                 
-                if (pos.X >= lengthX || pos.Y >= lengthY || pos.Z >= lengthZ)
-                {
-                    result++;
-                    continue;
-                }
-
-                if (model[pos.X, pos.Y, pos.Z] != '#')
-                {
-                    result++;
-                    continue;
-                }
+            if (pos.X >= lengthX || pos.Y >= lengthY || pos.Z >= lengthZ)
+            {
+                return true;
             }
-
-            return result;
+            
+            return false;
         }
 
         private void Print(char[,,] model)
