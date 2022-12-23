f0f5557d3b1ec1b57f0e9d0ca8ff1fea216513f9
SergeyRomanko <sergeyromanko@gmail.com>
2022-12-23 17:41:02 +0200

    Day 19.1

--- Adventofcode2022/Puzzles/Puzzle19/Puzzle19.cs
+++ Adventofcode2022/Puzzles/Puzzle19/Puzzle19.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Adventofcode2022.Common;
 
@@ -6,6 +7,15 @@ namespace Adventofcode2022.Puzzles
 {
     public class Puzzle19 : Puzzle
     {
+        private readonly IEnumerable<int[]> _options = new[]
+        {
+             new int[4] {0, 0, 0, 0},
+             new int[4] {1, 0, 0, 0},
+             new int[4] {0, 1, 0, 0},
+             new int[4] {0, 0, 1, 0},
+             new int[4] {0, 0, 0, 1},
+        };
+
         private struct Blueprint
         {
             public int Id;
@@ -17,6 +27,16 @@ namespace Adventofcode2022.Puzzles
             public int GeodeRobotCostObsidian;
         }
         
+        private class State
+        {
+            public int Minute;
+            public int[] Res;
+            public int[] Bot;
+            public List<State> Next;
+
+            public int Heuristics => Bot[0] + Bot[1] * 10 + Bot[2] * 100 + Bot[3] * 1000;
+        }
+
         public override string[] GetResults(IReadOnlyList<string> input)
         {
             var blueprints = input
@@ -49,27 +69,193 @@ namespace Adventofcode2022.Puzzles
 
         private int DoTask1(Blueprint blueprint)
         {
-            var ress = new int[4];
-            var bots = new int[4];
-
-            bots[0] = 1;
-            
-            for (var min = 0; min < 24; min++)
+            var minute1 = new State
             {
-                var delta = new int[4];
-
-                for (var i = 0; i < bots.Length; i++)
-                {
-                    ress[i] += bots[i];
-                }
+                Minute = 1,
+                Bot = new []{1,0,0,0},
+                Res = new []{0,0,0,0},
+            };
+            
+            var maxGeodes = Enumerable.Repeat(int.MaxValue, 1000).ToList();
+            
+            var max = 0;
+            
+            var candidates = new List<State> { minute1 };
+            while (candidates.Count > 0)
+            {
+                var candidate = candidates.Aggregate((a, b) => a.Heuristics > b.Heuristics ? a : b);
                 
-                for (var i = 0; i < delta.Length; i++)
+                candidates.Remove(candidate);
+                
+                var bestMinute2 = maxGeodes[candidate.Bot[Mineral.Geode] + 1];
+                if (candidate.Minute > bestMinute2)
                 {
-                    bots[i] += delta[i];
+                    continue;
                 }
+                maxGeodes[candidate.Bot[Mineral.Geode]] = Math.Min(candidate.Minute, maxGeodes[candidate.Bot[Mineral.Geode]]);
+                
+                var next = GetNextStates(candidate, blueprint).ToList();
+
+                candidate.Res[Mineral.Ore] += candidate.Bot[Mineral.Ore];
+                candidate.Res[Mineral.Clay] += candidate.Bot[Mineral.Clay];
+                candidate.Res[Mineral.Obsidian] += candidate.Bot[Mineral.Obsidian];
+                candidate.Res[Mineral.Geode] += candidate.Bot[Mineral.Geode];
+                
+                foreach (var nxt in next)
+                {
+                    nxt.Res[Mineral.Ore] += candidate.Bot[Mineral.Ore];
+                    nxt.Res[Mineral.Clay] += candidate.Bot[Mineral.Clay];
+                    nxt.Res[Mineral.Obsidian] += candidate.Bot[Mineral.Obsidian];
+                    nxt.Res[Mineral.Geode] += candidate.Bot[Mineral.Geode];
+                }
+
+                candidate.Next = next;
+                
+                candidates.InsertRange(0, candidate.Next);
+
+                max = Math.Max(max, candidate.Res[Mineral.Geode]);
             }
 
-            return ress[3];
+            //Console.WriteLine($"   {blueprint.Id}: {max}");
+            /*
+                1: 1
+                2: 3
+                3: 3
+                4: 1
+                5: 10
+                6: 3
+                7: 0
+                8: 3
+                9: 3
+                10: 2
+                11: 3
+                12: 9
+                13: 1
+                14: 15
+                15: 0
+                16: 1
+                17: 5
+                18: 0
+                19: 1
+                20: 0
+                21: 2
+                22: 0
+                23: 9
+                24: 1
+                25: 2
+                26: 3
+                27: 2
+                28: 0
+                29: 1
+                30: 0
+             */
+            
+            return max;
+        }
+
+        private IEnumerable<State> GetNextStates(State candidate, Blueprint blueprint)
+        {
+            if (candidate.Minute == 24)
+            {
+                yield break;
+            }
+
+            foreach (var option in _options)
+            {
+                var oreTotal = 
+                    blueprint.OreRobotCost         * option[Mineral.Ore] + 
+                    blueprint.ClayRobotCost        * option[Mineral.Clay] + 
+                    blueprint.ObsidianRobotCostOre * option[Mineral.Obsidian] +
+                    blueprint.GeodeRobotCostOre    * option[Mineral.Geode];
+
+                if (candidate.Res[Mineral.Ore] < oreTotal)
+                {
+                    continue;
+                }
+
+                var clayTotal = blueprint.ObsidianRobotCostClay * option[Mineral.Obsidian];
+                if (candidate.Res[Mineral.Clay] < clayTotal)
+                {
+                    continue;
+                }
+                
+                var obsidianTotal = blueprint.GeodeRobotCostObsidian * option[Mineral.Geode];
+                if (candidate.Res[Mineral.Obsidian] < obsidianTotal)
+                {
+                    continue;
+                }
+                
+                var next = new State
+                {
+                    Minute = candidate.Minute + 1,
+                    Bot = candidate.Bot.ToArray(),
+                    Res = candidate.Res.ToArray(),
+                };
+
+                next.Res[Mineral.Ore] -= oreTotal;
+                next.Res[Mineral.Clay] -= clayTotal;
+                next.Res[Mineral.Obsidian] -= obsidianTotal;
+
+                if (option[Mineral.Ore] > 0)      next.Bot[Mineral.Ore]++;
+                if (option[Mineral.Clay] > 0)     next.Bot[Mineral.Clay]++;
+                if (option[Mineral.Obsidian] > 0) next.Bot[Mineral.Obsidian]++;
+                if (option[Mineral.Geode] > 0)    next.Bot[Mineral.Geode]++;
+                
+                if (
+                    next.Bot[Mineral.Clay] == 0 &&
+                    next.Res[Mineral.Ore] >= blueprint.OreRobotCost &&
+                    next.Res[Mineral.Ore] >= blueprint.ClayRobotCost
+                )
+                {
+                    continue;
+                }
+
+                if (
+                    next.Bot[Mineral.Obsidian] == 0 &&
+                    next.Res[Mineral.Ore]  >= blueprint.ObsidianRobotCostOre &&
+                    next.Res[Mineral.Clay] >= blueprint.ObsidianRobotCostClay
+                    )
+                {
+                    continue;
+                }
+
+                if (
+                    next.Res[Mineral.Ore] >= blueprint.GeodeRobotCostOre &&
+                    next.Res[Mineral.Obsidian] >= blueprint.GeodeRobotCostObsidian)
+                {
+                    continue;
+                }
+                
+                if (
+                    next.Bot[Mineral.Ore] > blueprint.OreRobotCost &&
+                    next.Bot[Mineral.Ore] > blueprint.ClayRobotCost &&
+                    next.Bot[Mineral.Ore] > blueprint.ObsidianRobotCostOre &&
+                    next.Bot[Mineral.Ore] > blueprint.GeodeRobotCostOre
+                    )
+                {
+                    continue;
+                }
+                
+                if (next.Bot[Mineral.Clay] > blueprint.ObsidianRobotCostClay)
+                {
+                    continue;
+                }
+                
+                if (next.Bot[Mineral.Obsidian] > blueprint.GeodeRobotCostObsidian)
+                {
+                    continue;
+                }
+
+                yield return next;
+            }
+        }
+
+        private static class Mineral
+        {
+            public static readonly int Ore      = 0;
+            public static readonly int Clay     = 1;
+            public static readonly int Obsidian = 2;
+            public static readonly int Geode    = 3;
         }
     }
 }
