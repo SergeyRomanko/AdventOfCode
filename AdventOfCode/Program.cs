4dc2b215dedbbf6aa91ce506e12fb4d6970e9166
SergeyRomanko <sergeyromanko@gmail.com>
2025-12-03 16:37:35 +0200

    Add Year2025 Puzzle03 implementation and input
    Introduces Puzzle03 for Advent of Code 2025, including its solution logic and input file. Updates project and main program to register the new puzzle.

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -66,6 +66,7 @@ namespace AdventOfCode
         {
             new Year2025.Puzzle01(),
             new Year2025.Puzzle02(),
+            new Year2025.Puzzle03(),
         };
         
         public static void Main(string[] args)
---
+++ AdventOfCode/Year2025/Puzzle03/Puzzle03.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Year2025;
+
+public class Puzzle03 : Puzzle
+{
+    public override string[] GetResults(IReadOnlyList<string> input)
+    {
+        return new[]
+        {
+            DoThings(input,  2).Sum().ToString(),
+            DoThings(input, 12).Sum().ToString(),
+        };
+    }
+
+    private IEnumerable<long> DoThings(IReadOnlyList<string> input, int digits)
+    {
+        var result = new List<char>();
+        
+        foreach (var line in input)
+        {
+            var start = 0;
+            
+            for (var i = digits - 1; i >= 0; i--)
+            {
+                var span = line.AsSpan(start, line.Length - i - start);
+
+                var maxChar = Max(span);
+                
+                result.Add(maxChar);
+
+                start += span.IndexOf(maxChar) + 1;
+            }
+            
+            yield return long.Parse(result.ToArray());
+            
+            result.Clear();
+        }
+    }
+
+    private char Max(ReadOnlySpan<char> span)
+    {
+        if (span.IsEmpty)
+            throw new InvalidOperationException("Span is empty");
+
+        var max   = span[0];
+        
+        for (int i = 1; i < span.Length; i++)
+        {
+            if (span[i] > max)
+                max = span[i];
+        }
+        
+        return max;
+    }
+}
